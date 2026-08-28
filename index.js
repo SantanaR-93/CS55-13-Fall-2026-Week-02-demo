2167ea939b4e4ecc3fff09ebbda0d4390264e0c5
Robert Santana <santana.rob93@gmail.com>
2026-08-28 13:54:35 -0700

    complete demo

---
+++ index.js
@@ -0,0 +1,44 @@
+// CS55.13-Fall-2026-Week-02
+
+//use http package (shared code) from node.js
+const myhttp = require('http');
+
+//load the core node filesystem (fs) module, using js promises instead of callbacks
+const fs = require("fs").promises;
+
+//create a function to respond to http requests
+const requestListener =  function( myrequest, myresponse ) {
+        console.log( myrequest.url );
+
+        let mytext;
+        if (myrequest.url === '/') {
+            fs.readFile(__dirname + "/page.html").then(
+                contents => {
+                    myresponse.setHeader("Content-Type", "text/html; charset=UTF-8");
+                    myresponse.writeHead(200);
+                    myresponse.end(contents);
+                }
+            )
+        } else {
+            //if request url not root, return to json file
+            fs.readFile(__dirname + "/data.json").then(
+                contents => {
+                    myresponse.setHeader("Content-Type", "application/json; charset=UTF-8");
+                    myresponse.writeHead(200);
+                    myresponse.end(contents);
+                }
+            );
+        }
+
+    };
+
+//use http package createServer()
+//that runs a web server
+let myserver = myhttp.createServer(
+    // createServer() uses our function to run when a request comes in
+   requestListener
+);
+
+// ask http to start listening on a tcp port for incomoing http requests
+// listen() takes 2 args: l: tcp port number, string of the ip addreass to listen (0.0.0.0)
+myserver.listen( 8080, "127.0.0.1" );
